49cf89556b9bd29d33ac79e46fc8427cefa82118
Muhammed ahmed <muhameedahmed4472@gmail.com>
2025-05-15 22:24:22 +0300

    last commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';  // Changed here
+import { HashRouter } from 'react-router-dom';  
 import App from './App.jsx';
 import { AuthProvider } from './contexts/AuthContext';
 import { ToastContainer } from 'react-toastify';
@@ -9,7 +9,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>  {/* Use HashRouter instead of BrowserRouter */}
+    <HashRouter>  
       <AuthProvider>
         <App />
         <ToastContainer position="top-right" autoClose={3000} />
